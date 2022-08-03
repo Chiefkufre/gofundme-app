e9a00e55b27cf6ea32343a3c36df9909594d0ab7
chiefkufre <kufhub@gmail.com>
2022-08-03 15:49:05 -0700

    chore: update form.py with correct field types

--- src/forms.py
+++ src/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import FileField,SelectField, SubmitField, StringField,DateTimeField, BooleanField, PasswordField
+from wtforms import FileField,SelectField, SubmitField, StringField,DateTimeField, BooleanField, PasswordField, IntegerField
 from wtforms.validators import DataRequired, Email, URL, EqualTo, Regexp, Length
 from flask_wtf.file import FileField, FileAllowed, FileRequired
 from flask_ckeditor import CKEditorField
@@ -11,7 +11,7 @@ class SignUpForm(FlaskForm):
     confirmPassword = PasswordField("confirm password", validators=[DataRequired(), EqualTo("password", message="password not match")])
     userImage = FileField("profile Picture", validators=[FileAllowed(["jpg","png"]), FileRequired("file was empty")])
     country = StringField("country", validators=[DataRequired()])
-    post_code = StringField("postcode", validators=[DataRequired()])
+    post_code = IntegerField("postcode", validators=[DataRequired()])
     first_name = StringField("first name", validators=[DataRequired()])
     last_name = StringField("last name", validators=[DataRequired()])
     about = SelectField("Who are you raising the fund for?", validators=[DataRequired()],
@@ -21,7 +21,7 @@ class SignUpForm(FlaskForm):
                             ),
                             ("Someone else-You will invite a beneficiary to receive fund or distribute it yourself","Someone else-You will invite a beneficiary to receive fund or distribute it yourself")
                         ])
-    amount = StringField("Your starting goal", validators=[DataRequired()])
+    amount = IntegerField("Your starting goal", validators=[DataRequired()])
     submit = SubmitField('continue')
     
     class SignInForm(FlaskForm):
