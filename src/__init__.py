a885e25cb21cfa66817d4bf69795f75c1a328f03
chiefkufre <kufhub@gmail.com>
2022-08-04 08:59:09 -0700

    chore: register view context in app

--- src/__init__.py
+++ src/__init__.py
@@ -1,16 +1,18 @@
+import bcrypt
 from flask import Flask, jsonify, abort, redirect, render_template,flash, url_for
 from .models import setup_db
 from flask_ckeditor import CKEditor
+from flask_bcrypt import Bcrypt
 
 
+bcrypt = Bcrypt()
 def main_app():
     app = Flask(__name__)
+    bcrypt.init_app(app)
     setup_db(app)
     
-    @app.route("/")
-    def home():
-        
-        return render_template("pages/home.html")
-    
+   
+    from .views import views
+    app.register_blueprint(views, url_prefix=("/"))
     
     return app
