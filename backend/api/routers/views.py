6bdb1e372a7f5d5f26efd93af70f25024622cd9d
chiefkufre <samuelkufrewillie@gmail.com>
2023-01-27 23:06:37 +0100

    chore: add get campaign by id route

--- backend/api/routers/views.py
+++ backend/api/routers/views.py
@@ -1,27 +1,106 @@
-import email
-from hashlib import sha256
-from unicodedata import category
+import json 
 
 import validators
 
 import flask
-from flask import redirect, abort, Blueprint, flash, url_for, jsonify
+from flask import redirect, abort, Blueprint, flash, url_for, request, jsonify
 from api.models import User, Campaign, Donation, Message
-
+from api.utils.validators import  validate_title, validate_user
 
 
 views = Blueprint("views", __name__)
 
+
+    # id = db.Column(db.Integer, primary_key=True)
+    # title = db.Column(db.String(100), nullable=False)
+    # description = db.Column(db.String(500), nullable=False)
+    # goal = db.Column(db.Float, nullable=False)
+    # duration = db.Column(db.Integer, nullable=False)
+    # created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    # updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    # user_id = db.Column(db.Integer,db.ForeignKey('user.id'), nullable=False)
+    # donations = db.relationship('Donation', backref='campaign', lazy=True)
+
+    
 #   TODO: create campaigns
 #         POST / campaigns
+@views.post("/campaigns/create")
+def create_campaign():
 
+    data = request.get_json()
+   
+    title = data.get("title")
+    goal = data.get("goal")
+    description = data.get("description")
+    user_id = data.get("user_id")
+
+    if not all([title, goal, description, user_id]):
+        return jsonify({"message": "Missing required fields"}), 400
+    
+    try:
+         # Validate data
+        validate_user(user_id)
+
+        validate_title(title, description)
+    
+        campaign = Campaign(**data)
+        campaign.insert()
+
+        return jsonify({"message": "Campaign created successfully"}), 201
+
+    except ValueError as e:
+        return jsonify({"message": str(e)}), 400
+        campaign.rollback()
+
+    
+    return jsonify(
+        {
+            "Status": "success",
+            "id": campaign.id,
+            "title": campaign.title,
+            "goal": campaign.goal,
+            "description": campaign.description,
+            "created_at": campaign.created_at
+
+        })
+
+   
+
+    
 
 # TODO: Get all campaign models
     #  GET /campaigns/
+
+@views.get('/campaign')
+def getCampaign():
+
+    query = Campaign.query.all()
+
+    return json.loads(query)
      
 
-# TODO: Get all campaigns by on Id
+# TODO: Get all campaigns by  Id
 # GET /<campaign_id>
+
+@views.get('/campaign/<int:campaign_id>')
+def get_campaign_id(campaign_id):
+
+    campaign = Campaign.query.filter(Campaign.id == campaign_id).first()
+
+    if campaign != None:
+        return jsonify(
+            {
+                "Status": "success",
+                "id": campaign.id,
+                "title": campaign.title,
+                "goal": campaign.goal,
+                "description": campaign.description,
+                "created_at": campaign.created_at
+
+            })
+    else:
+        abort(400, description="user not found")
+
 
 #  TODO: update capaigns based on id
 #  PUT /<campaign_id>
