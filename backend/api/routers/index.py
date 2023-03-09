986af3c1a67d3e8eb3de7afd6da2b25c27e6e131
chiefkufre <samuelkufrewillie@gmail.com>
2023-03-09 23:57:11 +0100

    chore: link up static files

---
+++ backend/api/routers/index.py
@@ -0,0 +1,10 @@
+from flask import Blueprint, render_template, url_for
+
+
+index = Blueprint('index', __name__)
+
+
+# home view
+@index.get("/")
+def home():
+    return render_template("api/index.html")
