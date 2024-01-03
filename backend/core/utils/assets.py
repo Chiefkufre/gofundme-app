03824449df854c366be8de780acb2ed896f9e5d0
chiefkufre <samuelkufrewillie@gmail.com>
2024-01-03 04:11:43 +0100

    modify static file dir naming to meet app specification and assets bundle script

---
+++ backend/core/utils/assets.py
@@ -0,0 +1,50 @@
+from flask.ext.assets import Bundle, Environment
+from core.main import create_app_instance
+
+
+bundles = {
+
+    'api_js': Bundle(
+        'js/api.js',
+        output='gen/api.js'),
+
+    'api_css': Bundle(
+        'css/api.css',
+        output='gen/front.css'),
+
+    'front_js': Bundle(
+        'js/common.js',
+        'js/front.js',
+        output='gen/front.js'),
+
+    'front_css': Bundle(
+        'css/common.css',
+        'css/front.css',
+        output='gen/front.css'),
+
+    'admin_js': Bundle(
+        'js/common.js',
+        'js/admin.js',
+        output='gen/admin.js'),
+
+    'admin_css': Bundle(
+        'css/common.css',
+        'css/admin.css',
+        output='gen/admin.css'),
+
+    'dashboard_js': Bundle(
+        'js/common.js',
+        'js/dashboard.js',
+        output='gen/dashboard.js'),
+
+    'dashboard_css': Bundle(
+        'css/common.css',
+        'css/dashbord.css',
+        output='gen/dashboard.css'),
+    
+}
+
+assets = Environment(create_app_instance)
+
+assets.register(bundles)
+
