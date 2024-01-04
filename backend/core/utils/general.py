d34ab39e1c2874e5f8fd4545bde15c884284b9ed
chiefkufre <samuelkufrewillie@gmail.com>
2024-01-04 16:34:17 +0100

    chore: add mixin class to model

--- backend/core/utils/general.py
+++ backend/core/utils/general.py
@@ -26,34 +26,41 @@ def paginate(query, page=1, per_page=10):
 
 
 # Response formatting function for content negotiation
-def format_response(data, status_code=200, template_name=None):
+def format_response(response_data, status_code=200, template_name=None):
     if request.is_json:
-        return jsonify(data), status_code
-    elif template_name:
-        return render_template(template_name, data=data), status_code
+        return jsonify(response_data), status_code
     else:
-        # Default template if no specific template is provided
-        return render_template('default.html', data=data), status_code
+        return render_template(template_name + '.html', data=response_data), status_code
+   
     
 
 
 def generate_validator(model, create=False):
     class Validator:
         def __init__(self):
-            pass
+            self.model_fields = getattr(model, 'get_fields', lambda: [])()
 
-        def validate(self, data):
+
+        def validate_json_request(self, data):
             errors = {}
 
-            # Get the list of fields from the model
-            model_fields = getattr(model, 'get_fields', lambda: [])()
-
             # Check for required fields
-            for field in model_fields:
+            for field in self.model_fields:
                 if field not in data:
                     errors[field] = f"Field '{field}' is required."
 
             # Additional validation logic can be added here
+
+            return errors
+        
+        
+        def validate_form_request(self, form_data):
+            errors = {}
+
+            # Check for required form fields
+            for field in self.model_fields:
+                if field not in form_data:
+                    errors[field] = f"Field '{field}' is required in the form."
 
             return errors
 
