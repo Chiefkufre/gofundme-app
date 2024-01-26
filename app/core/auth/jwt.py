70c55e4baf25fc8a17e2b5752acd64af5ad2edaf
chiefkufre <samuelkufrewillie@gmail.com>
2024-01-26 12:30:33 +0100

    chore: configured jwt for auth

---
+++ app/core/auth/jwt.py
@@ -0,0 +1,25 @@
+from flask_jwt_extended import JWTManager, get_jwt_identity
+from core.models import User
+
+jwt = JWTManager(add_context_processor=True)
+
+
+@jwt.user_identity_loader
+def user_identity_lookup(user):
+    return {
+        'id': user.id,
+        'email': user.email,
+    }
+
+
+
+@jwt.user_lookup_loader
+def user_lookup_callback(_jwt_header, jwt_data):
+    identity = jwt_data["sub"]
+    user_id = jwt_data.get('id')
+
+    if user_id:
+        return User.query.get(user_id)
+    else:
+        return User.query.filter_by(id=identity).one_or_none()
+
